a20cfaa294e44469a253b852a7fd96ec9fce85d0
JHA <1004067303@qq.com>
2025-02-18 17:18:36 +0800

    minio使用

--- JavaBackground/SpringLearn/Spring-QuicklyStart/src/main/java/jha/spring/springquicklystart/controller/UploadController.java
+++ JavaBackground/SpringLearn/Spring-QuicklyStart/src/main/java/jha/spring/springquicklystart/controller/UploadController.java
@@ -1,6 +1,8 @@
 package jha.spring.springquicklystart.controller;
 
 
+import io.minio.MinioClient;
+import io.minio.UploadObjectArgs;
 import jha.spring.springquicklystart.pojo.Request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,10 +26,32 @@ public class UploadController {
         String newFileName = newName+filename.substring(filename.lastIndexOf("."));
         try {
            // file.transferTo(new File("D:\\JAVA\\resource\\uploadFile\\" + filename));//保存下来的文件名为源文件名
-            file.transferTo(new File("D:\\JAVA\\resource\\uploadFile\\" + newFileName));//保存下来的文件名为源文件名
-        } catch (IOException e) {
-            log.error("文件保存出错");
-        }
+            // file.transferTo(new File("D:\\JAVA\\resource\\uploadFile\\" + newFileName));//保存下来的文件名为源文件名
+
+
+            MinioClient minioClient = MinioClient.builder()
+                    .endpoint("http://169.254.64.130:9000/")
+                    .credentials("admin", "jha220717")
+                    .build();
+
+            minioClient.putObject(
+                    io.minio.PutObjectArgs.builder()
+                            .bucket("jha-test")
+                            .object(filename)
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build());
+               /* //本机测试使用
+                minioClient.uploadObject(
+                        UploadObjectArgs.builder()
+                                .bucket("jha-test")
+                                .object(objectName)
+                                .filename("D:/Users/Desktop/1.pdf")
+                                .build());*/
+                System.out.println("文件上传成功！");
+
+        } catch (Exception e) {
+        System.err.println("文件上传失败: " + e.getMessage());}
         return Request.success("文件保存成功");
     }
 }
--- JavaBackground/SpringLearn/Spring-QuicklyStart/src/test/java/jha/spring/springquicklystart/SpringQuicklyStartApplicationTests.java
+++ JavaBackground/SpringLearn/Spring-QuicklyStart/src/test/java/jha/spring/springquicklystart/SpringQuicklyStartApplicationTests.java
@@ -1,5 +1,10 @@
 package jha.spring.springquicklystart;
 
+import io.minio.GetPresignedObjectUrlArgs;
+import io.minio.MinioClient;
+import io.minio.UploadObjectArgs;
+import io.minio.errors.*;
+import io.minio.http.Method;
 import jha.spring.springquicklystart.Mapper.EmpMapper2;
 import jha.spring.springquicklystart.dao.EmpMapper;
 import jha.spring.springquicklystart.pojo.Emps;
@@ -9,12 +14,18 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootTest
 class SpringQuicklyStartApplicationTests {
     @Autowired
@@ -45,5 +56,51 @@ class SpringQuicklyStartApplicationTests {
         System.out.println("解析后的时间："+finalFormatter.format (LocalDateTime.parse(st, formatter)));
         }
 
-
+    @Test
+    public void uploadTest(){
+        MultipartFile file=null;
+        String objectName="1.pdf";
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint("http://169.254.64.130:9000/")
+                .credentials("admin", "jha220717")
+                .build();
+        try {
+            /*minioClient.putObject(
+                    io.minio.PutObjectArgs.builder()
+                            .bucket("jha-test")
+                            .object(objectName)
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build());*/
+            //本机测试使用
+            minioClient.uploadObject(
+                    UploadObjectArgs.builder()
+                            .bucket("jha-test")
+                            .object(objectName)
+                            .filename("D:/Users/Desktop/1.pdf")
+                            .build());
+            System.out.println("文件上传成功！");
+        } catch (Exception e) {
+            System.err.println("文件上传失败: " + e.getMessage());
+        }
+    }
+    @Test
+    public void previewUrlTest(){
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint("http://169.254.64.130:9000/")
+                .credentials("admin", "jha220717")
+                .build();
+        try {
+            String presignedObjectUrl = minioClient.getPresignedObjectUrl(
+                    GetPresignedObjectUrlArgs.builder()
+                            .method(Method.GET) // 请求方法
+                            .bucket("jha-test") // 存储桶名称
+                            .object("19_公告_page-0001.jpg") // 文件名称
+                            .expiry(20, TimeUnit.MINUTES) // 有效期
+                            .build());
+            System.out.println(presignedObjectUrl);
+        } catch (Exception e) {
+            System.out.printf("预览失败");
+        }
+    }
 }
